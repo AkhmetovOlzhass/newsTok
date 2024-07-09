8f9970025983831db69a1de9be5f53dbe99ab1ed
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2024-07-09 16:24:58 +0600

    addterms5

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,9 +15,9 @@ app.use((req, res, next) => {
     next();
 });
 
-const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "http://localhost:3000";
 
-// const BASE_URL = "https://news-tok.vercel.app"
+const BASE_URL = "https://news-tok.vercel.app"
 
 console.log(process.env.NODE_ENV);
 
