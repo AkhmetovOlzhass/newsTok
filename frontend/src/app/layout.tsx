b8fdfbe3d6f810502a50828788135a776b5d5d8d
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2024-07-15 22:30:18 +0600

    addsidebar2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import "./globals.css";
-import Sidebar from "./components/sidebar";
+import Sidebar from "./components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
