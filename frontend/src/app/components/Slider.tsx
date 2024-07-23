25602e0824e6cbee2d9f6704809eaa4a388c8e5b
sharn1999 <58104696+sharn1999@users.noreply.github.com>
2024-07-23 14:44:43 +0600

    changeplayer13

--- frontend/src/app/components/Slider.tsx
+++ frontend/src/app/components/Slider.tsx
@@ -133,9 +133,8 @@ const VerticalSlider: React.FC = () => {
                                         {preloadedIndexes.has(index) ? (
                                             <div className="player-wrapper">
                                                 <div
-                                                className=' relative'
                                                 onClick={handlePlayPause} >
-                                                    {playing ? null : <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="#fff" data-e2e="browse-video-play" className="player-pause"><use xlinkHref="#Play_Fill-3364e33a"></use></svg>}
+                                                    {playing ? null : <svg width="70" data-e2e="" height="70" viewBox="0 0 48 48" fill="#fff" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 8.77702C12 6.43812 14.5577 4.99881 16.5569 6.21266L41.6301 21.4356C43.5542 22.6038 43.5542 25.3962 41.6301 26.5644L16.5569 41.7873C14.5577 43.0012 12 41.5619 12 39.223V8.77702Z"></path></svg>}
                                                     <ReactPlayer
                                                         key={video.link}
                                                         loop={true}
